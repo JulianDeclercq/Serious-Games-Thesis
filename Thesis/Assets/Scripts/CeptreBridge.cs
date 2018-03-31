7b59a4cfa45c8dbf3b94cba4ceebdb3c8fbdb2d6
Julian Declercq <julian.declercq1@gmail.com>
2018-03-31 18:16:06 +0200

    Added Unity project.
    Basic executable running implemented.

---
+++ Thesis/Assets/Scripts/CeptreBridge.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Diagnostics;
+using System.IO;
+
+public class CeptreBridge : MonoBehaviour
+{
+    public Text testText;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            StartCeptre();
+    }
+
+    private void StartCeptre()
+    {
+        // Create the path to the executable
+        string path = Path.GetFullPath(@"..\Thesis\Assets\StreamingAssets\") + "ExeSimulator.exe";
+
+        // Specify the process
+        Process ceptreProcess = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = path,
+                Arguments = "",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            }
+        };
+
+        // Start the process
+        try
+        {
+            ceptreProcess.Start();
+        }
+        catch (Exception e)
+        {
+            print("Error: " + e.Message);
+            return;
+        }
+
+        // Read the output from the process
+        while (!ceptreProcess.StandardOutput.EndOfStream)
+            testText.text += ceptreProcess.StandardOutput.ReadLine() + '\n';
+    }
+}
